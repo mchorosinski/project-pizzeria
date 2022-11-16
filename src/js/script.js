a0934049d2aaf700c98942eda5622ad554bcc440
Mateusz <106282318+mchorosinski@users.noreply.github.com>
2022-11-16 20:42:02 +0200

    Add amount choosing

--- src/js/script.js
+++ src/js/script.js
@@ -47,7 +47,7 @@
     amountWidget: {
       defaultValue: 1,
       defaultMin: 1,
-      defaultMax: 9,
+      defaultMax: 10,
     }
   };
 
@@ -67,6 +67,7 @@
       thisProduct.getElements();
       thisProduct.initAccordion(); //! Chcemy, żeby produkty od razu, od samego początku mogły być zwijane/rozwijane (dlatego wywołanie znajduje się już w konstrukotrze).
       thisProduct.initOrderForm();
+      thisProduct.initAmountWidget();
       thisProduct.processOrder();
       console.log('new Product:', thisProduct);
     }
@@ -130,7 +131,11 @@
 
       //! Poniżej referencja do pojedynczego elementu o selektorze zapisanym w select.menuProduct.imageWrapper, czyli: '.product__images'
       thisProduct.imageWrapper = thisProduct.element.querySelector(select.menuProduct.imageWrapper);
-      c('thisProduct.imageWrapper:', thisProduct.imageWrapper);
+      //c('thisProduct.imageWrapper:', thisProduct.imageWrapper);
+
+      //! Właściwość thisProduct.amountWidgetElem, kórej wartością jest referencja do elementu o selektorze select.menuProduct.amountWidget
+      thisProduct.amountWidgetElem = thisProduct.element.querySelector(select.menuProduct.amountWidget); // '.widget-amount'
+      c('thisProduct.amountWidgetElem:', thisProduct.amountWidgetElem);
 
     }
 
@@ -188,16 +193,16 @@
 
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive);
 
-        c(thisProduct.element);
+        //c(thisProduct.element);
 
       });
     }
 
     initOrderForm() {
       const thisProduct = this;
-      c('initOrderForm:', thisProduct.initOrderForm);
+      //c('initOrderForm:', thisProduct.initOrderForm);
 
-      thisProduct.form.addEventListener('submit', function(event){
+      thisProduct.form.addEventListener('submit', function(event) {
         event.preventDefault();
         thisProduct.processOrder();
       });
@@ -254,7 +259,7 @@
           // find an image in type of category-option -> .paramId-optionId
 
           const optionImage = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
-          c('optionImage:', optionImage);
+          //c('optionImage:', optionImage);
 
           // check if in formData exists a category name (param), and then (if yes) does it include a proper (checked/selected) option
           //! 'params' to sauce, toppings, crust; 'options' to tomato, olives, standard
@@ -296,8 +301,119 @@
         }
       }
 
+      /* multiply price by amount */
+
+      //! Tuż przed wyświetleniem ceny obliczonej z uwzględnieniem opcji produktu, pomnożymy ją przez ilość sztuk wybraną w widgecie.
+      price *= thisProduct.amountWidget.value;
+
       // update calculated price in the HTML
+
       thisProduct.priceElem.innerHTML = price;
+    }
+
+    //! Metoda initAmountWidget jest odpowiedzialna za utworzenie nowej instancji klasy AmountWidget i zapisywanie jej we właściwości produktu
+    //! Od razu przekazujemy do konstruktora referencję do naszego diva z inputem i buttonami tak, jak oczekiwała na to klasa AmountWidget
+    initAmountWidget() {
+      const thisProduct = this;
+
+      thisProduct.amountWidget = new AmountWidget(thisProduct.amountWidgetElem);
+
+      //! Nasłuchiwanie customowego eventu = Nasłuchuje na element `thisProduct.amountWidgetElem` i na zdarzenie `updated`.
+      //? Dlaczego nasłuchujemy właśnie na ten element? Bo to na nim emitowaliśmy nasz event (thisWidget.element to referencja do tego samego identycznego elementu co thisProduct.amountWidgetElem).
+      thisProduct.amountWidgetElem.addEventListener('updated', function() {
+        thisProduct.processOrder();
+      });
+    }
+  }
+
+  class AmountWidget {
+
+    constructor(element) {
+      const thisWidget = this;
+      thisWidget.getElements(element);
+      thisWidget.setValue(thisWidget.input.value);
+      //! metoda `initActions` uruchamia się automatycznie, od razu po utworzeniu instancji.
+      thisWidget.initActions();
+
+      //c('AmounWidget:', thisWidget);
+      //c('constructor arguments:', element);
+    }
+
+    getElements(element){
+      const thisWidget = this;
+
+      thisWidget.element = element;
+      thisWidget.value = settings.amountWidget.defaultValue;
+      thisWidget.input = thisWidget.element.querySelector(select.widgets.amount.input);
+      thisWidget.linkDecrease = thisWidget.element.querySelector(select.widgets.amount.linkDecrease);
+      thisWidget.linkIncrease = thisWidget.element.querySelector(select.widgets.amount.linkIncrease);
+    }
+
+    setValue(value) {
+      const thisWidget = this;
+
+      //! Każdy input, nawet o typie `number`, zawsze zwraca wartość w formacie tekstowym.
+      //! Nawet wpisanie więc 10 da nam nie liczbę 10, a tekst '10'. parseInt zadba o konwersję takiej przykładowej '10' do liczby 10.
+      const newValue = parseInt(value); // przekonwertowanie argumentu na liczbę
+
+      /* TODO: Add validation */
+
+      //! Sprawdzamy, czy wartość, która przychodzi do funkcji, jest inna niż ta, która jest już aktualnie w `thisWidget.value` oraz ustawiamy zakres akceptowalnych wartości, tj. liczba sztuk: 1-10.
+      if(thisWidget.value !== newValue && !isNaN(newValue) && newValue >= settings.amountWidget.defaultMin && newValue <= settings.amountWidget.defaultMax) { // `!==` ozanacza różne wartości i typy danych; `!isNaN` nie jest nullem, ponieważ jeśli parseInt natrafi na tekst, którego nie da się skonwertować na liczbę (np. abc), to zwróci null
+        thisWidget.value = newValue; // zapisuje we właściwości thisWidget.value wartość przekazanego argumentu
+      }
+
+      thisWidget.input.value = thisWidget.value; // aktualizuje wartość samego inputu
+
+      //! Wywołanie metody - zadbać o to, aby uruchamiała się dopiero wtedy, kiedy nowa wartość (czyli `input.value`), którą chcemy ustawić, faktycznie jest poprawna.
+      //! Event `updated` uruchomi się przy zmianie wartości, ale tylko na taką, która wciąż będzie poprawna.
+      thisWidget.announce();
+
+    }
+
+    initActions() {
+      const thisWidget = this;
+
+      //! dla thisWidget.input listener eventu to `change`,
+      //! zaś jego handler używa metody `setValue` z argumentem z wartością inputa
+      thisWidget.input.addEventListener('change', function() {
+        //c('changed');
+
+        thisWidget.setValue(thisWidget.input.value);
+      });
+
+      //! dla thisWidget.linkDecrease listener eventu to `click`,
+      //! zaś jego handler powstrzymuje domyślną akcję dla tego eventu,
+      //! oraz używa metody `setValue` z argumentem `thisWidget.value` pomniejszonym o 1
+      thisWidget.linkDecrease.addEventListener('click', function(event) {
+        //c('clicked');
+
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value -1);
+
+      });
+
+      //! dla thisWidget.linkDecrease listener eventu to `click`,
+      //! zaś jego handler powstrzymuje domyślną akcję dla tego eventu,
+      //! oraz używa metody `setValue` z argumentem `thisWidget.value` powiększonym o 1
+      thisWidget.linkIncrease.addEventListener('click', function(event) {
+        //c('clicked');
+
+        event.preventDefault();
+        thisWidget.setValue(thisWidget.value +1);
+
+      });
+    }
+
+    //! CUSTOMOWY EVENT!
+    announce() {
+      const thisWidget = this;
+
+      //! Metody `announce` tworzy instancje klasy Event, wbudowanej w silnik JS. Jest to klasa odpowiedzialna za stworzenie obiektu "eventu". Następnie, ten event zostanie wyemitowany na kontenerze widgetu.
+      //! CZYLI: użytkownik klika gdzieś na stronie, to przeglądarka robi dokładnie to samo co my teraz.
+      //! Również tworzy event click w podobny sposób przy użyciu klasy Event, a następnie emituje go na tym klikniętym elemencie za pomocą metody `dispatchEvent` - później koniecznie jest nasłuchiwanie tego customowego zdarzenia w `initAmountWidget`.
+      const event = new Event('updated');
+      thisWidget.element.dispatchEvent(event);
     }
   }
 
